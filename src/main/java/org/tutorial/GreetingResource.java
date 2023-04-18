b276c59d47edfc44a5dd2b8ff593c10e6855c6e9
Miguel Angel reyes <mareyes46@misena.edu.co>
2023-04-18 10:32:20 -0500

    hello word ok

--- src/main/java/org/tutorial/GreetingResource.java
+++ src/main/java/org/tutorial/GreetingResource.java
@@ -8,9 +8,10 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    //comando para correr la app mvn quarkus:dev
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello from RESTEasy Reactive developer mode";
     }
 }
